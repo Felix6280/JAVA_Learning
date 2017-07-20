aa3a7872630de1029ed883f19af18c07ebced0aa
Bak Jun-hyeong <zxc1628@gmail.com>
2017-07-20 10:37:41 +0900

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,11 @@
+public class Datatype {
+        public static void main(String[]args){
+                System.out.println(1+2);
+                System.out.println(1.2+1.3);
+
+                System.out.println('c');
+                System.out.println("String");
+                System.out.println('char'); // Error
+                System.out.println("c");
+        }
+}
